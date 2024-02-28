23d3a0dc920032c4137d6ce17248edd9a5d47197
chutima puthachon <chutima.p8@gmail.com>
2024-02-28 17:53:36 +1100

    progress

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   # post '/lists', to: 'lists#create'
 
   resources :lists, only: [:index, :create, :show, :new] do
-  resources :bookmarks, only: [:new, :create, :destroy]
+    resources :bookmarks, only: [:new, :create]
   end
 end
